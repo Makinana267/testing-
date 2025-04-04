4e75ce94c1de056883fbbbfd82c72ad0a3c02a59
Makinana267 <iviwe.makinana@sambeconsulting.com>
2025-04-04 10:17:11 +0200

    Update create_db_and_table.sql

--- create_db_and_table.sql
+++ create_db_and_table.sql
@@ -1,23 +1,39 @@
 -- Create the database if it doesn't exist
-USE master
-GO
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Autotest_IM_27March')
-BEGIN
-    CREATE DATABASE Autotest_IM_27March;
-END
+
+BEGIN TRY
+    -- Use master to check and create the database if not exists
+    USE master;
+    IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Autotest_IM_27March')
+    BEGIN
+        CREATE DATABASE Autotest_IM_27March;
+    END
+END TRY
+BEGIN CATCH
+    PRINT 'Error creating database: ' + ERROR_MESSAGE();
+END CATCH
 GO
 
--- Switch to the database
-USE Autotest_IM_27March;
+BEGIN TRY
+    -- Switch to the database
+    USE Autotest_IM_27March;
+END TRY
+BEGIN CATCH
+    PRINT 'Error switching to database: ' + ERROR_MESSAGE();
+END CATCH
 GO
 
--- Create the user table if it doesn't exist
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'user')
-BEGIN
-    CREATE TABLE [user] (
-        Name NVARCHAR(50),
-        Surname NVARCHAR(50),
-        Email NVARCHAR(100)
-    );
-END
+BEGIN TRY
+    -- Create the user table if it doesn't exist
+    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'user')
+    BEGIN
+        CREATE TABLE [user] (
+            Name NVARCHAR(50),
+            Surname NVARCHAR(50),
+            Email NVARCHAR(100)
+        );
+    END
+END TRY
+BEGIN CATCH
+    PRINT 'Error creating table: ' + ERROR_MESSAGE();
+END CATCH
 GO
